f612aefaa7e259b00e469341bcf0beb5f4d78def
Mahnoor Babar <mahnoorbabar@MahnoornoMacBook-Air.local>
2024-10-20 20:16:30 +0500

    quiz commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import react, {useState} from 'react'
 
 export default function App() {
+const [count, setCount] = useState (0);
+
+const incrementButton =()=>{
+
+  setCount(count+1);
+};
+const decrementButton =()=>{
+
+  setCount(count-1);
+};
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{message}</Text>
+      <Button title="Increment" onPress={incrementButton}/>
+      <Button title="Decrement" onPress={decrementButton}/>
     </View>
+
   );
 }
 
@@ -17,4 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:24
+  }
 });
